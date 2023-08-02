a775a8fb1fab18fde8f315ba54239d2fdb048395
flowcore-platform <gitops@flowcore.io>
2023-08-02 09:57:30 +0100

    fix: added missing log level configuration to simple logger

--- src/logger/factories/simple-logger.factory.ts
+++ src/logger/factories/simple-logger.factory.ts
@@ -4,6 +4,7 @@ import { LoggerTransportFactory } from "./logger-transport.factory";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const createSimpleLogger = (configuration: any) => {
   return WinstonModule.createLogger({
+    level: configuration?.logger?.level || "info",
     transports: [
       configuration?.logger?.pretty
         ? LoggerTransportFactory.prettyFormat()
